6e27197f12da75d2a01ff8b72eb1f7c0ca400f64
jasonscharff <jasonscharff@gmail.com>
2016-02-20 02:30:27 -0500

    Create project and install some libraries.

---
+++ ios/Hymn/Hymn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hymn
+//
+//  Created by Jason Scharff on 2/20/16.
+//  Copyright © 2016 Jason Scharff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/Hymn/Hymn/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Hymn
+//
+//  Created by Jason Scharff on 2/20/16.
+//  Copyright © 2016 Jason Scharff. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  self.view.backgroundColor = [UIColor whiteColor];
+  
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
