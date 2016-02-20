e505c886318c32d4d79c3ea939c219c614a48334
jasonscharff <jasonscharff@gmail.com>
2016-02-20 08:00:33 -0500

    Add AFNetworking.

--- ios/Hymn/Pods/Target Support Files/Pods-Hymn/Pods-Hymn-frameworks.sh
+++ ios/Hymn/Pods/Target Support Files/Pods-Hymn/Pods-Hymn-frameworks.sh
@@ -82,8 +82,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Hymn/AFNetworking.framework"
   install_framework "Pods-Hymn/SocketIOClientSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Hymn/AFNetworking.framework"
   install_framework "Pods-Hymn/SocketIOClientSwift.framework"
 fi
