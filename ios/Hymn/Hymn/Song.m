8eb085b2788cd1ca84c028c6eaa80573ae2e7a37
jasonscharff <jasonscharff@gmail.com>
2016-02-20 20:56:17 -0500

    Bug fix.

--- ios/Hymn/Hymn/Song.m
+++ ios/Hymn/Hymn/Song.m
@@ -14,7 +14,16 @@
   self.name = dictionary[@"name"];
   self.spotifyURI = dictionary[@"uri"];
   NSArray *images = dictionary[@"album"][@"images"];
-  self.imageURL = images[1][@"url"];
+  if(images.count == 0) {
+    self.imageURL = @"";
+  }
+  else if (images.count == 1) {
+    self.imageURL = images[0][@"url"];
+  }
+  else {
+    self.imageURL = images[1][@"url"];
+  }
+  
   self.artistName = dictionary[@"artists"][0][@"name"]; //Note, there could be multiple artists. I'm just using the first one listed.
 }
 
